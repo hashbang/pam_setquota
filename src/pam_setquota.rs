0d2661b8590dc8a5b2811f059e62016c05e9f8c4
The Fox in the Shell <KellerFuchs@hashbang.sh>
2016-08-17 16:57:57 +0200

    Comment the code

--- src/pam_setquota.rs
+++ src/pam_setquota.rs
@@ -26,27 +26,38 @@ pub extern fn pam_sm_open_session(pamh: &module::PamHandleT, flags: PamFlag,
 
     let args = unsafe { translate_args(argc, argv) };
 
-
+    // We use the Result<_, pam::constant> monad:
+    //  if something triggers an error, it has an associated pam::constant value
+    //  and processing stop (we go to .unwrap_or_else() and log the error)
     (mdo! {
+        // Get the username from PAM
         username =<< module::get_user(pamh, None)
             .map_err(|e| (e, "Failed to get username"));
 
+        // Get the user object from the passwd db
         user =<< users::get_user_by_name(&username)
             .ok_or((PAM_USER_UNKNOWN, "Unknown user"));
 
+        // If this is a system user (uid < 1000), bail out early with PAM_SUCCESS
         () =<< if user.uid() < 1000 { Err((PAM_SUCCESS, "")) } else { Ok(()) };
 
 
+        // Parse the module's arguments.
+        // It is done late to avoid erroring out if the user has uid < 1000
         quota =<< parse_args(args)
             .map_err(|s| (PAM_SESSION_ERR, &*format!("Failed to parse {}", s)));
 
 
+        // Get the user's homedir mountpoint.
+        // Somehow, this requires unwrapping twice (yay for silly APIs!)
         home_opt =<< get_mount(user.home_dir())
             .or(Err((PAM_SESSION_ERR, "Couldn't get the homedir's mountpoint")));
 
         home =<< home_opt
             .ok_or((PAM_SESSION_ERR, "Couldn't get the homedir's mountpoint"));
 
+
+        // Perform the actual quotactl(2) call
         () =<< quotactl_set(quota::USRQUOTA,
                             &home.file,
                             user.uid() as i32,
@@ -56,6 +67,8 @@ pub extern fn pam_sm_open_session(pamh: &module::PamHandleT, flags: PamFlag,
         ret Ok(PAM_SUCCESS)
     }).unwrap_or_else(|(e, msg)|
                       if e != PAM_SUCCESS {
+                          // We cheerfully ignore errors in logging to syslog,
+                          // since there is nothing we can do about it.
                           mdo! {
                               writer =<< syslog::unix(Facility::LOG_AUTH);
                               result =<< writer.send_3164(Severity::LOG_ALERT, &format!("pam_setquota: {}", msg));
@@ -67,16 +80,22 @@ pub extern fn pam_sm_open_session(pamh: &module::PamHandleT, flags: PamFlag,
 }
 
 
+// parse_args returns either a quota::Dqblk struct
+//  or the string that failed to parse.
 fn parse_args(args: Vec<String>) -> Result<quota::Dqblk, String> {
     use nom::{alpha,digit};
     use std::{i32,str};
     use nix::sys::quota::quota::{QuotaValidFlags,QIF_BLIMITS,QIF_ILIMITS};
 
+    // The default quota value
+    // It isn't quota::Dqblk::default() directly because
+    //  the documentation doesn't state what the default value is.
     let quota0 = quota::Dqblk {
         valid: QuotaValidFlags::empty(),
         .. quota::Dqblk::default()
     };
 
+    // A parser (and converter) for “([a-z]+)=([0-9])+,([0-9])+”
     named!(arg<&[u8], Option<(&[u8], u64, u64)> >,
            chain!(tag: alpha ~
                   char!('=') ~
@@ -97,6 +116,8 @@ fn parse_args(args: Vec<String>) -> Result<quota::Dqblk, String> {
            )
     );
 
+    // We fold over the arguments, updating the quota value as we go.
+    // Again, the Result<> monad is used to error-out early.
     return args.iter().fold(Ok(quota0),
               |res, s| {
                   use mdo::result::{bind,ret};
@@ -129,6 +150,7 @@ fn parse_args(args: Vec<String>) -> Result<quota::Dqblk, String> {
 }
 
 #[no_mangle]
+// Closing the session involves no special work.
 pub extern fn pam_sm_close_session(pamh: *mut module::PamHandleT, flags: PamFlag,
                                    argc: c_int, argv: *const *const c_char
                                    ) -> PamResultCode {
@@ -136,6 +158,8 @@ pub extern fn pam_sm_close_session(pamh: *mut module::PamHandleT, flags: PamFlag
 }
 
 
+// Arcane magic to turn (argc,argv) into a Vec<String>.
+// Please set your syntax coloring to octarine.
 unsafe fn translate_args(argc: c_int, argv: *mut *const c_char) -> Vec<String> {
     use std::ffi;
     let v = Vec::<*const c_char>::from_raw_parts(argv, argc as usize, argc as usize);
