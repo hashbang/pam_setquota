7b3418b67ae3e712c4fb1d20664d648c554fd561
Mark Sinclair II <125axel125@gmail.com>
2017-03-14 22:33:22 -0500

    Removed mdo dependency and usage

--- src/pam_setquota.rs
+++ src/pam_setquota.rs
@@ -1,8 +1,6 @@
 #![feature(libc)]
 #![allow(unused_variables)]
 extern crate libc;
-#[macro_use]
-extern crate mdo;
 extern crate nix;
 extern crate users;
 extern crate pam;
@@ -25,68 +23,70 @@ pub extern "C" fn pam_sm_open_session(pamh: &module::PamHandleT,
                                       argc: c_int,
                                       argv: *mut *const c_char)
                                       -> PamResultCode {
-    use mdo::result::{bind, ret};
+
+    let args = unsafe { translate_args(argc, argv) };
+
+    let log_err_and_return = |(e, msg): (PamResultCode, Cow<str>)| -> PamResultCode {
+        if e != PAM_SUCCESS {
+            // We cheerfully ignore errors in logging to syslog,
+            // since there is nothing we can do about it.
+            let _ = syslog::unix(Facility::LOG_AUTH)
+                .and_then(|writer| {
+                    writer.send_3164(Severity::LOG_ALERT,
+                                    &format!("pam_setquota: {}", msg))
+                });
+        };
+
+        e
+    };
+
+    //  if something triggers an error, it has an associated pam::constant value
+    //  and processing stop (we go to .unwrap_or_else() and log the error)
+    open_session(pamh, flags, args)
+            .unwrap_or_else(log_err_and_return)
+}
+
+pub fn open_session(pamh: &module::PamHandleT,
+                                      flags: PamFlag,
+                                      args: Vec<String>)
+                                      -> Result<PamResultCode, (PamResultCode, Cow<str>)> {
     use users::os::unix::UserExt;
     use mnt::get_mount;
     use nix::sys::quota::quotactl_set;
 
-    let args = unsafe { translate_args(argc, argv) };
+    // Get the username from PAM
+    let username = module::get_user(pamh, None)
+        .map_err(|e| (e, Cow::from("")))?;
 
-    // We use the Result<_, pam::constant> monad:
-    //  if something triggers an error, it has an associated pam::constant value
-    //  and processing stop (we go to .unwrap_or_else() and log the error)
-    (mdo! {
-        // Get the username from PAM
-        username =<< module::get_user(pamh, None)
-            .map_err(|e| (e, Cow::from("Failed to get username")));
+    // Get the user object from the passwd db
+    let user = users::get_user_by_name(&username)
+        .ok_or((PAM_USER_UNKNOWN, Cow::from("Unknown user")))?;
 
-        // Get the user object from the passwd db
-        user =<< users::get_user_by_name(&username)
-            .ok_or((PAM_USER_UNKNOWN, Cow::from("Unknown user")));
+    // If this is a system user (uid < 1000), bail out early with PAM_SUCCESS
+    if user.uid() < 1000 {
+        return Err((PAM_SUCCESS, Cow::from("")));
+    }
 
-        // If this is a system user (uid < 1000), bail out early with PAM_SUCCESS
-        () =<< if user.uid() < 1000 { Err((PAM_SUCCESS, Cow::from(""))) } else { Ok(()) };
+    // Parse the module's arguments.
+    // It is done late to avoid erroring out if the user has uid < 1000
+    let quota = parse_args(&args)
+        .map_err(|s| (PAM_SESSION_ERR, Cow::from(format!("Failed to parse {}", s))))?;
 
+    // Get the user's homedir mountpoint.
+    // Somehow, this requires unwrapping twice (yay for silly APIs!)
+    let home = get_mount(user.home_dir())
+        .map_err(|_| (PAM_SESSION_ERR, Cow::from("Couldn't get the homedir's mountpoint")))?
+        .ok_or((PAM_SESSION_ERR, Cow::from("Couldn't get the homedir's mountpoint")))?;
 
-        // Parse the module's arguments.
-        // It is done late to avoid erroring out if the user has uid < 1000
-        quota =<< parse_args(&args)
-            .map_err(|s| (PAM_SESSION_ERR, Cow::from(format!("Failed to parse {}", s))));
-
-
-        // Get the user's homedir mountpoint.
-        // Somehow, this requires unwrapping twice (yay for silly APIs!)
-        home_opt =<< get_mount(user.home_dir())
-            .or(Err((PAM_SESSION_ERR, Cow::from("Couldn't get the homedir's mountpoint"))));
-
-        home =<< home_opt
-            .ok_or((PAM_SESSION_ERR, Cow::from("Couldn't get the homedir's mountpoint")));
-
-
-        // Perform the actual quotactl(2) call
-        () =<< quotactl_set(quota::USRQUOTA,
+    // Perform the actual quotactl(2) call
+    quotactl_set(quota::USRQUOTA,
                             &home.file,
                             user.uid() as i32,
                             &quota)
-            .or(Err((PAM_SESSION_ERR, Cow::from("Failed to set quota"))));
+            .or(Err((PAM_SESSION_ERR, Cow::from("Failed to set quota"))))?;
 
-        ret Ok(PAM_SUCCESS)
-    })
-            .unwrap_or_else(|(e, msg)| {
-                if e != PAM_SUCCESS {
-                    // We cheerfully ignore errors in logging to syslog,
-                    // since there is nothing we can do about it.
-                    mdo! {
-                              writer =<< syslog::unix(Facility::LOG_AUTH);
-                              result =<< writer.send_3164(Severity::LOG_ALERT,
-                                                          &format!("pam_setquota: {}", msg));
-                              ret Ok(result)
-                          };
-                };
-                e
-            })
+    Ok(PAM_SUCCESS)
 }
-
 
 // parse_args returns either a quota::Dqblk struct
 //  or the string that failed to parse.
@@ -109,33 +109,34 @@ fn parse_args<'a>(args: &'a Vec<String>) -> Result<quota::Dqblk, Cow<'a, str>> {
 
 
     // We fold over the arguments, updating the quota value as we go.
-    // Again, the Result<> monad is used to error-out early.
-    args.iter().fold(Ok(quota0), |res, s| {
-        use mdo::result::{bind, ret};
+    // Again, the Result<> is used to error-out early.
+    args.iter().fold(Ok(quota0), |res, s| ->  Result<quota::Dqblk, Cow<'a, str>> {
         use nom::IResult::Done;
-        mdo! {
-                             quota0 =<< res;
-                             // TODO: This is horrible; check why parse cannot be deconstructed
-                             parse =<< match arg(s) {
-                                 Done(_, o) => Ok(o),
-                                 _ => Err(Cow::from(s.as_str()))
-                             };
-                             ret match parse.0 {
-                                 "blocks" => Ok(quota::Dqblk {
-                                     bsoftlimit: parse.1,
-                                     bhardlimit: parse.2,
-                                     valid:      quota0.valid | QIF_BLIMITS,
-                                     .. quota0
-                                 }),
-                                 "inodes" => Ok(quota::Dqblk {
-                                     isoftlimit: parse.1,
-                                     ihardlimit: parse.2,
-                                     valid:      quota0.valid | QIF_ILIMITS,
-                                     .. quota0
-                                 }),
-                                 _ => Err(Cow::from(s.as_str()))
-                             }
-                         }
+
+        let quota0 = res?;
+
+        let parse = match arg(s) {
+            Done(_, o) => Ok(o),
+            _ => Err(Cow::from(s.as_str())),
+        }?;
+
+        let (type_, soft, hard) = parse;
+
+        match type_ {
+            "blocks" => Ok(quota::Dqblk {
+                bsoftlimit: soft,
+                bhardlimit: hard,
+                valid: quota0.valid | QIF_BLIMITS,
+                .. quota0
+            }),
+            "inodes" => Ok(quota::Dqblk {
+                isoftlimit: soft,
+                ihardlimit: hard,
+                valid: quota0.valid | QIF_ILIMITS,
+                .. quota0
+            }),
+            _ => Err(Cow::from(s.as_str()))
+        }
     })
 }
 
