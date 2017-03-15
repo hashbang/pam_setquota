d8e85750690954cdad2ae094602bf27a72a59906
Mark Sinclair II <125axel125@gmail.com>
2017-03-14 22:34:47 -0500

    Ran rustfmt

--- src/pam_setquota.rs
+++ src/pam_setquota.rs
@@ -30,11 +30,9 @@ pub extern "C" fn pam_sm_open_session(pamh: &module::PamHandleT,
         if e != PAM_SUCCESS {
             // We cheerfully ignore errors in logging to syslog,
             // since there is nothing we can do about it.
-            let _ = syslog::unix(Facility::LOG_AUTH)
-                .and_then(|writer| {
-                    writer.send_3164(Severity::LOG_ALERT,
-                                    &format!("pam_setquota: {}", msg))
-                });
+            let _ = syslog::unix(Facility::LOG_AUTH).and_then(|writer| {
+                writer.send_3164(Severity::LOG_ALERT, &format!("pam_setquota: {}", msg))
+            });
         };
 
         e
@@ -42,25 +40,23 @@ pub extern "C" fn pam_sm_open_session(pamh: &module::PamHandleT,
 
     //  if something triggers an error, it has an associated pam::constant value
     //  and processing stop (we go to .unwrap_or_else() and log the error)
-    open_session(pamh, flags, args)
-            .unwrap_or_else(log_err_and_return)
+    open_session(pamh, flags, args).unwrap_or_else(log_err_and_return)
 }
 
 pub fn open_session(pamh: &module::PamHandleT,
-                                      flags: PamFlag,
-                                      args: Vec<String>)
-                                      -> Result<PamResultCode, (PamResultCode, Cow<str>)> {
+                    flags: PamFlag,
+                    args: Vec<String>)
+                    -> Result<PamResultCode, (PamResultCode, Cow<str>)> {
     use users::os::unix::UserExt;
     use mnt::get_mount;
     use nix::sys::quota::quotactl_set;
 
     // Get the username from PAM
-    let username = module::get_user(pamh, None)
-        .map_err(|e| (e, Cow::from("")))?;
+    let username = module::get_user(pamh, None).map_err(|e| (e, Cow::from("")))?;
 
     // Get the user object from the passwd db
-    let user = users::get_user_by_name(&username)
-        .ok_or((PAM_USER_UNKNOWN, Cow::from("Unknown user")))?;
+    let user =
+        users::get_user_by_name(&username).ok_or((PAM_USER_UNKNOWN, Cow::from("Unknown user")))?;
 
     // If this is a system user (uid < 1000), bail out early with PAM_SUCCESS
     if user.uid() < 1000 {
@@ -110,7 +106,7 @@ fn parse_args<'a>(args: &'a Vec<String>) -> Result<quota::Dqblk, Cow<'a, str>> {
 
     // We fold over the arguments, updating the quota value as we go.
     // Again, the Result<> is used to error-out early.
-    args.iter().fold(Ok(quota0), |res, s| ->  Result<quota::Dqblk, Cow<'a, str>> {
+    args.iter().fold(Ok(quota0), |res, s| -> Result<quota::Dqblk, Cow<'a, str>> {
         use nom::IResult::Done;
 
         let quota0 = res?;
@@ -123,19 +119,23 @@ fn parse_args<'a>(args: &'a Vec<String>) -> Result<quota::Dqblk, Cow<'a, str>> {
         let (type_, soft, hard) = parse;
 
         match type_ {
-            "blocks" => Ok(quota::Dqblk {
-                bsoftlimit: soft,
-                bhardlimit: hard,
-                valid: quota0.valid | QIF_BLIMITS,
-                .. quota0
-            }),
-            "inodes" => Ok(quota::Dqblk {
-                isoftlimit: soft,
-                ihardlimit: hard,
-                valid: quota0.valid | QIF_ILIMITS,
-                .. quota0
-            }),
-            _ => Err(Cow::from(s.as_str()))
+            "blocks" => {
+                Ok(quota::Dqblk {
+                    bsoftlimit: soft,
+                    bhardlimit: hard,
+                    valid: quota0.valid | QIF_BLIMITS,
+                    ..quota0
+                })
+            }
+            "inodes" => {
+                Ok(quota::Dqblk {
+                    isoftlimit: soft,
+                    ihardlimit: hard,
+                    valid: quota0.valid | QIF_ILIMITS,
+                    ..quota0
+                })
+            }
+            _ => Err(Cow::from(s.as_str())),
         }
     })
 }
@@ -158,8 +158,8 @@ unsafe fn translate_args(argc: c_int, argv: *mut *const c_char) -> Vec<String> {
     let v = Vec::<*const c_char>::from_raw_parts(argv, argc as usize, argc as usize);
     v.into_iter()
         .filter_map(|arg| {
-                        let bytes = ffi::CStr::from_ptr(arg).to_bytes();
-                        String::from_utf8(bytes.to_vec()).ok()
-                    })
+            let bytes = ffi::CStr::from_ptr(arg).to_bytes();
+            String::from_utf8(bytes.to_vec()).ok()
+        })
         .collect()
 }
